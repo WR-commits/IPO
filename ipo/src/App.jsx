882153adea771d863c9778f68f7f06e29fcf29b0
WR-commits <ribeirowaner4@gmail.com>
2026-06-08 10:53:22 +0000

    comitadaaaaaaaaaaaa

--- ipo/src/App.jsx
+++ ipo/src/App.jsx
@@ -9,17 +9,64 @@ function App() {
           <div className="navbar-nav">
             <Link className="nav-link" to="/clientes">Clientes</Link>
             <Link className="nav-link" to="/veiculos">Veículos</Link>
-            <Link className="nav-link" to="/inspecoes">Inspeções</Link>            
+            <Link className="nav-link" to="/inspecoes">Inspeções</Link>
           </div>
         </div>
       </nav>
       <div className="container mt-4">
+
         <Routes>
           <Route path="/" element={<Inicio />} />
           <Route path="/clientes" element={<ClientesList />} />
           <Route path="/veiculos" element={<VeiculosList />} />
           <Route path="/inspecoes" element={<InspecoesList />} />
         </Routes>
+
+        <div> 
+<button className="btn btn-dark"><i className="fa fa-plus"></i> Novo Cliente</button>
+<button className="btn btn-dark"><i className="fa-pencli"></i> Atualizar</button>
+        </div>
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Codigo</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Morada</th>
+              <th scope="col">Nif</th>
+              <th scope="col">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Luis</td>
+              <td>Lisboa</td>
+              <td>1234</td>
+              <td><button className="btn btn-dark"><i className="fa fa-eye"></i></button></td>
+              <td><button className="btn btn-dark"><i className="fa fa-pencil"></i></button></td> 
+              <td><button className="btn btn-dark"><i className="fa fa-table"></i></button></td>                              
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Dias</td>
+              <td>porto</td>
+              <td>1241</td>
+              <td><button className="btn btn-dark"><i className="fa fa-eye"></i></button></td>
+              <td><button className="btn btn-dark"><i className="fa fa-pencil"></i></button></td>  
+              <td><button className="btn btn-dark"><i className="fa fa-table"></i></button></td>                               
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>larry</td>
+              <td>braga</td>
+              <td>21321</td>
+              <td><button class="btn btn-dark"><i className="fa fa-eye"></i></button></td>
+              <td><button className="btn btn-dark"><i className="fa fa-pencil"></i></button></td>  
+              <td><button className="btn btn-dark"><i className="fa fa-table"></i></button></td>                               
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
@@ -28,6 +75,7 @@ function App() {
 function Inicio() {
   return (<h2>Bem-vindo ao sistema IPO</h2>);
 }
+
 function ClientesList() {
   return (<h2>Página de Clientes</h2>);
 }
